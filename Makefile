bd144825a9eacdb5df66101daac700d9a3799b56
dromadaire <dromadaire@localhost.localdomain>
2019-09-03 08:37:45 +0200

    V0.0

---
+++ Makefile
@@ -0,0 +1,22 @@
+all: Final Test
+
+Final: fonctions.o main.o
+	gcc -lm -Wall -g -std=c99 fonctions.o main.o -o Final
+
+Test: fonctions.o tests.o
+	gcc -lm -Wall -g -std=c99 fonctions.o tests.o -o Test
+
+fonctions.o: fonctions.c header.h
+	gcc -lm -Wall -g -std=c99 -c fonctions.c -o fonctions.o
+
+main.o: main.c
+	gcc -lm -Wall -g -std=c99 -c main.c -o main.o
+
+exec:
+	rm -rf *.o
+	rm -rf *.psh
+
+purge:
+	rm -rf *.o
+	rm -rf *.psh
+	rm -rf Final Test
---
+++ fonctions.c
@@ -0,0 +1,33 @@
+#include "header.h"
+
+void swap(int* a, int* b)
+{
+  int c = *a;
+  *a = *b;
+  *b = c;
+}
+
+
+void affiche(int* a, int n){
+	int i;
+	for (i = 0 ; i < n ; i++){
+		printf("%d ", a[i]);
+	}
+	printf("\n\n");
+}
+
+
+int compare(int* a, int *b, int n){
+	int i;
+	for(i=0; i < n ; i++){
+		if (a[i] != b[i]){
+			printf("Comparison FAILED at index %d\n\n", i);
+			return i;
+		}
+	}
+	printf("Comparison OK\n\n");
+	return -1;
+}
+
+
+void merge (int* tab, int* tmp, int left, int mid, int right, int* cnt);
